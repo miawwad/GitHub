5705a25f5dc7c26238e3e428f2d754a445c0b631
Mo <hamodaawwad55@gmail.com>
2024-03-11 21:33:17 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+function Header(){
+  return(
+    <Text>This is a Header</Text>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
